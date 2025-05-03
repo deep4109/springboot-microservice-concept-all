c803db935865a18a1caf4268cb24d3a3238f2a7e
deep4109 <singhdeepankar41@gmail.com>
2025-05-03 20:40:03 +0530

    RbbitMQ

--- microservice_resilient_circuit_breaker/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ microservice_resilient_circuit_breaker/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -27,6 +27,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.concurrent.TimeoutException;
+
 /**
  * @author Eazy Bytes
  */
@@ -203,8 +205,9 @@ public class AccountsController {
     )
     @Retry(name = "getBuildInfo",fallbackMethod = "getBuildInfoFallback")
     @GetMapping("/build-info")
-    public ResponseEntity<String> getBuildInfo() {
+    public ResponseEntity<String> getBuildInfo() throws TimeoutException {
         logger.debug("getBuildInfo() method Invoked");
+       // throw new TimeoutException();
         return ResponseEntity
                     .status(HttpStatus.OK)
                     .body(buildVersion);
